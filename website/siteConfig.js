5f7b46c2586273f21a1ecb448ce37bb52e0af736
Phillip Smith <blueshiftps@gmail.com>
2019-02-26 20:18:10 -0500

    moving to org homepage

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -30,7 +30,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'docs',
+  projectName: 'algorithmic-freelancers.github.io',
   organizationName: 'algorithmic-freelancers',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
