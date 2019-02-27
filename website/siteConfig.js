cadc278ceb7bd232856b05a63e8e85000674f08d
Phillip Smith <blueshiftps@gmail.com>
2019-02-26 19:35:43 -0500

    Added placeholder docs

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -38,8 +38,8 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'doc1', label: 'Docs'},
-    {doc: 'doc4', label: 'API'},
+    {doc: 'mechanics/overview', label: 'Docs'},
+    {doc: 'todo', label: 'Contributing'},
     {page: 'help', label: 'Help'},
     {blog: true, label: 'Blog'},
   ],
